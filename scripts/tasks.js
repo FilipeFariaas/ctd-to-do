f1cca6a14b839c269874160d783b058464e78d45
Bruno Furukawa <bruno.b.furukawa@gmail.com>
2022-04-17 05:34:00 -0300

    inclui um skeleton basico no load da tela tasks.html Refatorei as funcoes tambem. A logica de colocar eventlisteners atraves do foreach estava incluindo mais de um evento por botao. Inclui a chamada das funcoes no "CreateTaskTemplate" E refatorei as "funcoes deleteTAsk" e "completeTask" Faltou refatorar o "editTask" Fiz o push disso num novo branch brunoSkeleton pra vc dar uma avaliada se quer dar o merge depois.

--- scripts/tasks.js
+++ scripts/tasks.js
@@ -13,71 +13,74 @@ const API_URL = `https://ctd-todo-api.herokuapp.com/v1`;
 const closeEditModal = (parent, child) => parent.removeChild(child);
 
 const deleteTask = (id) => {
-console.log(id);
-let config = {
-method: "DELETE",
-headers: {
-  "Content-type": "application/json",
-  Authorization: loggedUser
-}
-};
+  console.log(id);
+  let config = {
+    method: "DELETE",
+    headers: {
+      "Content-type": "application/json",
+      Authorization: loggedUser
+    }
+  };
 
-fetch(`${API_URL}/tasks/${id}`, config)
-.then((response) => {
-  return response.json();
-})
-.catch((err) => console.log(err));
+  fetch(`${API_URL}/tasks/${id}`, config)
+    .then((response) => {
+      //return response.json();
+      let cbtb = document.querySelector(`#_${id}`);
+      cbtb.remove();
+    })
+    .catch((err) => console.log(err));
+    
 };
 
 const saveChanges = (id, newTitle) => {
-const config = {
-method: "PUT",
-headers: {
-  "Content-type": "application/json",
-  Authorization: loggedUser
-},
-body: JSON.stringify({
-  description: newTitle,
-  completed: false
-})
-};
+  const config = {
+    method: "PUT",
+    headers: {
+      "Content-type": "application/json",
+      Authorization: loggedUser
+    },
+    body: JSON.stringify({
+      description: newTitle,
+      completed: false
+    })
+  };
 
-fetch(`${API_URL}/tasks/${id}`, config)
-.then((response) => {
-  return response.json();
-})
-.catch((err) => console.log(err));
+  fetch(`${API_URL}/tasks/${id}`, config)
+    .then((response) => {
+      return response.json();
+    })
+    .catch((err) => console.log(err));
 };
 
 const getTaskData = (id) => {
-const config = {
-method: "GET",
-headers: {
-  "Content-type": "application/json",
-  Authorization: loggedUser
-}
-};
+  const config = {
+    method: "GET",
+    headers: {
+      "Content-type": "application/json",
+      Authorization: loggedUser
+    }
+  };
 
-fetch(`${API_URL}/tasks/${id}`, config)
-.then((reponse) => {
-  return reponse.json();
-})
-.then((data) => {
-  const { id } = data;
-  const { description } = data;
+  fetch(`${API_URL}/tasks/${id}`, config)
+    .then((reponse) => {
+      return reponse.json();
+    })
+    .then((data) => {
+      const { id } = data;
+      const { description } = data;
 
-  editTask(id, description);
+      editTask(id, description);
 
-  return data;
-})
-.catch((err) => console.log(err));
+      return data;
+    })
+    .catch((err) => console.log(err));
 };
 
 const editTask = (id, taskTitle) => {
-const container = document.querySelector(`.container`);
-const editTaskModal = document.createElement(`div`);
-editTaskModal.classList.add(`edit-task-modal`);
-editTaskModal.innerHTML += `
+  const container = document.querySelector(`.container`);
+  const editTaskModal = document.createElement(`div`);
+  editTaskModal.classList.add(`edit-task-modal`);
+  editTaskModal.innerHTML += `
 <form class="edit-task">
   <button id="close-edit-modal" class="btn-close">
     <span class="lnr lnr-cross"></span>
@@ -93,116 +96,160 @@ editTaskModal.innerHTML += `
 </form>
 `;
 
-container.appendChild(editTaskModal);
+  container.appendChild(editTaskModal);
 
-const btnCloseModal = document.querySelector(`#close-edit-modal`);
-btnCloseModal.addEventListener(`click`, (e) => {
-e.preventDefault();
+  const btnCloseModal = document.querySelector(`#close-edit-modal`);
+  btnCloseModal.addEventListener(`click`, (e) => {
+    e.preventDefault();
 
-container.removeChild(editTaskModal);
-});
+    container.removeChild(editTaskModal);
+  });
 
-const currentTaskTitle = document.querySelector(`#_${id} .nome`);
-const inputEditTask = document.querySelector(`#edit-task-input`);
-const btnSaveEdit = document.querySelector(`#save-change`);
-const btnCancelEdit = document.querySelector(`#cancel-change`);
+  const currentTaskTitle = document.querySelector(`#_${id} .nome`);
+  const inputEditTask = document.querySelector(`#edit-task-input`);
+  const btnSaveEdit = document.querySelector(`#save-change`);
+  const btnCancelEdit = document.querySelector(`#cancel-change`);
 
-inputEditTask.addEventListener(`keypress`, (key) => {
-if (key.key === `Enter`) {
-  const newTitle = inputEditTask.value;
-  currentTaskTitle.innerText = newTitle;
+  inputEditTask.addEventListener(`keypress`, (key) => {
+    if (key.key === `Enter`) {
+      const newTitle = inputEditTask.value;
+      currentTaskTitle.innerText = newTitle;
 
-  saveChanges(id, newTitle);
-  // closeEditModal(container, editTaskModal);
-}
-});
+      saveChanges(id, newTitle);
+      // closeEditModal(container, editTaskModal);
+    }
+  });
 
-btnSaveEdit.addEventListener(`click`, (e) => {
-e.preventDefault();
+  btnSaveEdit.addEventListener(`click`, (e) => {
+    e.preventDefault();
 
-const newTitle = inputEditTask.value;
-currentTaskTitle.innerText = newTitle;
+    const newTitle = inputEditTask.value;
+    currentTaskTitle.innerText = newTitle;
 
-saveChanges(id, newTitle);
-closeEditModal(container, editTaskModal);
-});
+    saveChanges(id, newTitle);
+    closeEditModal(container, editTaskModal);
+  });
 
-btnCancelEdit.addEventListener(`click`, (e) => {
-e.preventDefault();
+  btnCancelEdit.addEventListener(`click`, (e) => {
+    e.preventDefault();
 
-closeEditModal(container, editTaskModal);
-});
+    closeEditModal(container, editTaskModal);
+  });
 };
 
-const completeTask = () => {
-const btnCompleteTask = document.querySelectorAll(`.not-done`);
+const completeTask = (id) => {
+    console.log(id);
 
-btnCompleteTask.forEach((btn) => {
-btn.addEventListener(`click`, (e) => {
-  e.preventDefault();
-  e.stopPropagation();
+    const task = document.querySelector(`#_${id}`);
+    const taskId = id;
+    const taskTitle = document.querySelector(`#_${id} .nome`).innerText;
+    const taskCreation = document.querySelector(`#_${id} .timestamp`).innerText;
 
-  console.log(`clicked`);
+    task.classList.toggle(`tarefa--pendente`);
+    task.classList.toggle(`tarefa--concluida`);
 
-  const task = btn.parentElement;
-  const taskId = task.getAttribute(`id`).slice(1);
-  const taskTitle = btn.nextElementSibling.children[0].innerText;
-  const taskCreation = btn.nextElementSibling.children[1].innerText;
+    const taskIsComplete = task.classList.contains(`tarefa--concluida`);
 
-  task.classList.toggle(`tarefa--pendente`);
-  task.classList.toggle(`tarefa--concluida`);
-
-  const taskIsComplete = task.classList.contains(`tarefa--concluida`);
-
-  console.log(task);
-
-  const taskObj = {
-    id: taskId,
-    description: taskTitle,
-    createdAt: taskCreation,
-    completed: taskIsComplete
-  };
-
-  task.remove();
-
-  renderTask(taskObj);
-
-  const config = {
-    method: "PUT",
-    headers: {
-      "Content-type": "application/json",
-      Authorization: loggedUser
-    },
-    body: JSON.stringify({
+    const taskObj = {
+      id: taskId,
       description: taskTitle,
-      completed: taskIsComplete ? true : false
-    })
-  };
+      createdAt: taskCreation,
+      completed: taskIsComplete
+    };
 
-  fetch(`${API_URL}/tasks/${taskId}`, config)
-    .then((response) => {
-      return response.json();
-    })
-    .then((response) => {
-      return response;
-    })
-    .catch((err) => console.log(err));
-});
-});
-};
+    console.log(taskObj);
+
+    task.remove();
+
+    renderTask(taskObj);
+
+    const config = {
+      method: "PUT",
+      headers: {
+        "Content-type": "application/json",
+        Authorization: loggedUser
+      },
+      body: JSON.stringify({
+        description: taskTitle,
+        completed: taskIsComplete ? true : false
+      })
+    };
+
+    fetch(`${API_URL}/tasks/${taskId}`, config)
+      .then((response) => {
+        return response.json();
+      })
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((err) => console.log(err));
+}
+
+// const completeTask = () => {
+//   const btnCompleteTask = document.querySelectorAll(`.not-done`);
+
+//   btnCompleteTask.forEach((btn) => {
+//     btn.addEventListener(`click`, async (e) => {
+//       e.preventDefault();
+//       e.stopPropagation();
+
+//       console.log(`clicked`);
+
+//       const task = btn.parentElement;
+//       const taskId = task.getAttribute(`id`).slice(1);
+//       const taskTitle = btn.nextElementSibling.children[0].innerText;
+//       const taskCreation = btn.nextElementSibling.children[1].innerText;
+
+//       task.classList.toggle(`tarefa--pendente`);
+//       task.classList.toggle(`tarefa--concluida`);
+//       const taskIsComplete = task.classList.contains(`tarefa--concluida`);
+//       const taskObj = {
+//         id: taskId,
+//         description: taskTitle,
+//         createdAt: taskCreation,
+//         completed: taskIsComplete
+//       };
+//       console.log(task);
+
+//       task.remove();
+
+//       renderTask(taskObj);
+
+//       const config = {
+//         method: "PUT",
+//         headers: {
+//           "Content-type": "application/json",
+//           Authorization: loggedUser
+//         },
+//         body: JSON.stringify({
+//           description: taskTitle,
+//           completed: taskIsComplete ? true : false
+//         })
+//       };
+
+//       fetch(`${API_URL}/tasks/${taskId}`, config)
+//         .then((response) => {
+//           return response.json();
+//         })
+//         .then((response) => {
+//           console.log(response);
+//         })
+//         .catch((err) => console.log(err));
+//     });
+//   });
+// };
 
 const createTaskTemplate = (id, title, creation, completed) => {
-const taskTemplate = `
-<li id=_${id} class="tarefa tarefa--${
-completed ? "concluida" : "pendente"
-}">
-  <button id="edit-task">
+  const taskTemplate = `
+<li id=_${id} class="tarefa tarefa--${completed ? "concluida" : "pendente"
+    }">
+  <button id="edit-task" onclick="editTask(${id})">
     <span class="lnr lnr-pencil"></span>
   </button>
-  <button id="deleteTask">
+  <button id="deleteTask" onclick="deleteTask(${id})">
     <span class="lnr lnr-cross"></span>
   </button>
-  <div class="not-done"></div>
+  <div class="not-done" onclick="completeTask(${id})"></div>
   <div class="descricao">
     <p class="nome">${title}</p>
     <p class="timestamp">${new Date(creation).toLocaleString()}</p>
@@ -210,117 +257,121 @@ completed ? "concluida" : "pendente"
 </li>
 `;
 
-return taskTemplate;
+  return taskTemplate;
 };
 
 const renderTask = (task) => {
-const { completed } = task;
+  const { completed } = task;
 
-if (completed) {
-tasksCompleted.innerHTML += createTaskTemplate(
-  task.id,
-  task.description,
-  task.createdAt,
-  task.completed
-);
-} else {
-tasksPending.innerHTML += createTaskTemplate(
-  task.id,
-  task.description,
-  task.createdAt,
-  task.completed
-);
-}
+  if (completed) {
+    tasksCompleted.innerHTML += createTaskTemplate(
+      task.id,
+      task.description,
+      task.createdAt,
+      task.completed
+    );
+  } else {
+    tasksPending.innerHTML += createTaskTemplate(
+      task.id,
+      task.description,
+      task.createdAt,
+      task.completed
+    );
+  }
 
-inputNewTask.value = "";
+  inputNewTask.value = "";
 
-const btnDeleteTask = document.querySelectorAll(`#deleteTask`);
+  // const btnDeleteTask = document.querySelectorAll(`#deleteTask`);
 
-btnDeleteTask.forEach((btn) => {
-btn.addEventListener(`click`, (e) => {
-  e.preventDefault();
+  // btnDeleteTask.forEach((btn) => {
+    
+  //   btn.addEventListener(`click`, (e) => {
+  //     e.preventDefault();
+  //     console.log('delete clicked')
+  //     const taskId = btn.parentElement.getAttribute("id").slice(1);
+  //     deleteTask(taskId);
+  //     btn.parentElement.remove();
+  //   });
+  // });
 
-  const taskId = btn.parentElement.getAttribute("id").slice(1);
-  deleteTask(taskId);
-  btn.parentElement.remove();
-});
-});
+  // const btnEditTask = document.querySelectorAll(`#edit-task`);
 
-const btnEditTask = document.querySelectorAll(`#edit-task`);
+  // btnEditTask.forEach((btn) => {
+  //   btn.addEventListener(`click`, (e) => {
+  //     e.preventDefault();
+  //     const taskId = btn.parentElement.getAttribute("id").slice(1);
 
-btnEditTask.forEach((btn) => {
-btn.addEventListener(`click`, (e) => {
-  e.preventDefault();
+  //     getTaskData(taskId);
+  //   });
+  // });
 
-  const taskId = btn.parentElement.getAttribute("id").slice(1);
-
-  getTaskData(taskId);
-});
-});
-
-completeTask();
+  //completeTask();
 };
 
 const loadUserTasks = () => {
-let config = {
-method: "GET",
-headers: {
-  "Content-type": "application/json",
-  Authorization: loggedUser
-}
-};
+  tasksPending.innerHTML = '<div class="skeletonLoading"></div>'
+  tasksCompleted.innerHTML = '<div class="skeletonLoading"></div>'
+  let config = {
+    method: "GET",
+    headers: {
+      "Content-type": "application/json",
+      Authorization: loggedUser
+    }
+  };
 
-fetch(`${API_URL}/tasks`, config)
-.then((response) => {
-  return response.json();
-})
-.then((response) => {
-  response.forEach((task) => {
-    renderTask(task);
-  });
-})
-.catch((err) => console.log(err));
+  fetch(`${API_URL}/tasks`, config)
+    .then((response) => {
+      return response.json();
+    })
+    .then((response) => {
+      tasksPending.innerHTML = "";
+      tasksCompleted.innerHTML = "";
+      response.forEach((task) => {
+        renderTask(task);
+      });
+    })
+    .catch((err) => console.log(err));
 };
 
 const validateAccess = () => {
-if (localStorage.getItem(`jwt`)) {
-loadUserTasks();
-} else {
-window.location = "../index.html";
-}
+  if (localStorage.getItem(`jwt`)) {
+    loadUserTasks();
+  } else {
+    window.location = "../index.html";
+  }
 };
 
 btnEndSession.addEventListener(`click`, (e) => {
-e.preventDefault();
+  e.preventDefault();
 
-localStorage.removeItem(`jwt`);
-window.location = "../index.html";
-console.log(`clicked`);
+  localStorage.removeItem(`jwt`);
+  window.location = "../index.html";
+  console.log(`clicked`);
 });
 
 btnAddTask.addEventListener(`click`, (e) => {
-e.preventDefault();
+  e.preventDefault();
 
-let config = {
-method: "POST",
-headers: {
-  "Content-type": "application/json",
-  Authorization: loggedUser
-},
-body: JSON.stringify({
-  description: inputNewTask.value,
-  completed: false
-})
-};
+  let config = {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json",
+      Authorization: loggedUser
+    },
+    body: JSON.stringify({
+      description: inputNewTask.value,
+      completed: false
+    })
+  };
 
-fetch(`${API_URL}/tasks`, config)
-.then((response) => {
-  return response.json();
-})
-.then((response) => {
-  renderTask(response);
-})
-.catch((err) => console.log(err));
+  fetch(`${API_URL}/tasks`, config)
+    .then((response) => {
+      return response.json();
+    })
+    .then((response) => {
+      renderTask(response);
+    })
+    .catch((err) => console.log(err));
 });
 
 window.onload = validateAccess();
